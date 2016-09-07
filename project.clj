cba1328a4cc5f808ab69dfb74c265285a998b90d
Jonathan Beaumont <jonathan.beaumont@thomsonreuters.com>
2016-09-07 19:45:19 -0400

    add project file

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject 1p-clojure-template "0.1.0-SNAPSHOT"
+    :description "Clojure microservice template"
+      :url "http://projectne.thomsonreuters.com"
+        :dependencies  [[org.clojure/clojure "1.5.1"]])
