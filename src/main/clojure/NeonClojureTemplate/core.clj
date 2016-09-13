a31dfce010e23ff8b2c33f611e6eca2cc78ac818
Jonathan Beaumont <jonathan.beaumont@thomsonreuters.com>
2016-09-13 17:05:32 -0400

    add more imports

--- src/main/clojure/NeonClojureTemplate/core.clj
+++ src/main/clojure/NeonClojureTemplate/core.clj
@@ -1,9 +1,14 @@
 (ns neonClojureTemplate.core
-  (import 
-    com.thomsonreuters.client.profile.impl.ProfileServiceClientImpl) 
+  (:import (com.thomsonreuters.client.profile.impl ProfileServiceClientImpl) 
+           (io.netty.buffer ByteBuf)
+           (java.nio.charset Charset)
+          )
   (:gen-class))
 
+(def testTruid "ee3faaf5-1000-4cb5-9a29-88468106e1f8")
 (def profileClient (ProfileServiceClientImpl.))
+(def byteBuf (.execute (.getUser profileClient testTruid)))
+(def userJson (.toString byteBuf (.forName Charset "UTF-8")))
 
 
 (def l (list 1 2 3 4 5))
